1fbe19f6fa4b3f92f132e4fc51e1bdb453095ceb
Kristian Kime <kristian.kime@gmail.com>
2017-09-20 21:46:59 -0400

    starting on graphs in inputs

---
+++ public/javascripts/artclod/artclod-graphs.js
@@ -0,0 +1,52 @@
+//http://stackoverflow.com/questions/6525538/convert-utc-date-time-to-local-date-time-using-javascript
+if (!ARTC) {
+    var ARTC = {};
+}
+
+ARTC.insertGraphO = function (params) {
+    ARTC.insertGraph(params.id, params.func, params.glider, params.xMin, params.xMax, params.yMin, params.yMax, params.xPixSize, params.yPixSize);
+};
+
+ARTC.insertGraph = function (id, func, glider, xMin, xMax, yMin, yMax, xPixSize, yPixSize) {
+    glider    = typeof glider    !== 'undefined' ? glider    : false;
+    xMin      = typeof xMin      !== 'undefined' ? xMin      : -11;
+    xMax      = typeof xMax      !== 'undefined' ? xMax      :  11;
+    yMin      = typeof yMin      !== 'undefined' ? yMin      : -11;
+    yMax      = typeof yMax      !== 'undefined' ? yMax      :  11;
+    xPixSize  = typeof xPixSize  !== 'undefined' ? xPixSize  : 300;
+    yPixSize  = typeof yPixSize  !== 'undefined' ? yPixSize  : 300;
+
+    var xRange = xMax - xMin;
+    var yRange = yMax - yMin;
+
+    var board = JXG.JSXGraph.initBoard(id, {axis: true, boundingbox:[xMin,yMax,xMax,yMin], originX: xPixSize/2, originY: yPixSize/2, unitX: xPixSize / xRange, unitY: yPixSize / yRange, showCopyright: false});
+    board.suspendUpdate();
+    var g = board.create('functiongraph', [func, xMin, xMax], {strokeWidth: 3});
+
+    if(glider) {
+        var glider = board.create('glider', [g]);
+
+        var getMouseCoords = function(e, i) { /* http://jsxgraph.uni-bayreuth.de/wiki/index.php/Browser_event_and_coordinates */
+            var cPos = board.getCoordsTopLeftCorner(e, i),
+                absPos = JXG.getPosition(e, i),
+                dx = absPos[0]-cPos[0],
+                dy = absPos[1]-cPos[1];
+
+            return new JXG.Coords(JXG.COORDS_BY_SCREEN, [dx, dy], board);
+        }
+
+        var down = function(e)  {
+            var i;
+            if (e[JXG.touchProperty]) {
+                i = 0; // index of the finger that is used to extract the coordinates
+            }
+            var coords = getMouseCoords(e, i);
+
+            glider.moveTo([coords.usrCoords[1], coords.usrCoords[2]]);
+        }
+
+        board.on('down', down);
+    }
+    board.unsuspendUpdate();
+    return board;
+};
--- public/javascripts/artclod/artclod-markdown.js
+++ public/javascripts/artclod/artclod-markdown.js
@@ -5,5 +5,37 @@ if (!ARTC) {
 // https://github.com/showdownjs/showdown
 ARTC.markdown = function (text) {
     var converter = new showdown.Converter();
-    return converter.makeHtml(text);
+
+
+    // store all the matches we find here
+    var replaces = {};
+    // We're looking for this
+    var pattern = /\$g\$.*?\$g\$/
+
+    // Our initial variables
+    var i = 0;
+    var index = "$g" + i + "g$";
+    var res = text.match(pattern, index);
+    res = (res == null ? null : res[0]);
+    text = text.replace(pattern, index);
+
+    while(res != null) { // stop when we fail to find a match
+        // We have a match so update matches
+        replaces[index] = res;
+
+        // Find the next potential match
+        res = text.match(pattern, index);
+        res = (res == null ? null : res[0]);
+        text = text.replace(pattern, index);
+
+        // update the index
+        i++;
+        index = "$g" + i + "g$";
+    }
+
+    // console.log(replaces);
+
+    var markdown = converter.makeHtml(text);
+
+    return markdown;
 }
