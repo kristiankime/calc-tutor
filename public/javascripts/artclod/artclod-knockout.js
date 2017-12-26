c4d3dde5777084ad0e7ca63ae8872c8c57bc9752
Kristian Kime <kristian.kime@gmail.com>
2017-12-26 16:52:28 -0500

    date bindings now default back to current time if they are sent to nonsense values

--- public/javascripts/artclod/artclod-knockout.js
+++ public/javascripts/artclod/artclod-knockout.js
@@ -50,10 +50,31 @@ ko.bindingHandlers.mathjax = {
 };
 
 // https://stackoverflow.com/questions/18016718/using-knockout-js-how-do-bind-a-date-property-to-a-html5-date-picker#18058410
+ko.bindingHandlers.datetimeLocalPicker = {
+    init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+        // Register change callbacks to update the model if the control changes.
+        ko.utils.registerEventHandler(element, "change", function () {
+            var value = valueAccessor();
+            value(new Date(element.value));
+        });
+    },
+    // Update the control whenever the view model changes
+    update: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+        var value = valueAccessor();
+        if(value().isValid()) {
+            element.value = value().toLocalISOStringNoZ();
+        } else {
+            var now = new Date();
+            now.setMinutes(0, 0, 0); // doesn't need to be accurate down to the millisecond...
+            value(now);
+            element.value = now.toLocalISOStringNoZ();
+        }
+    }
+};
+
 // ko.bindingHandlers.datetimeLocalPicker = {
 //     init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
-//         // Register change callbacks to update the model
-//         // if the control changes.
+//         // Register change callbacks to update the model if the control changes.
 //         ko.utils.registerEventHandler(element, "change", function () {
 //             var value = valueAccessor();
 //             value(new Date(element.value));
@@ -62,23 +83,6 @@ ko.bindingHandlers.mathjax = {
 //     // Update the control whenever the view model changes
 //     update: function (element, valueAccessor, allBindingsAccessor, viewModel) {
 //         var value = valueAccessor();
-//         // var vTmp = value();
 //         element.value = value().toLocalISOStringNoZ();
 //     }
 // };
-
-ko.bindingHandlers.datetimeLocalPicker = {
-    init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
-        // Register change callbacks to update the model
-        // if the control changes.
-        ko.utils.registerEventHandler(element, "change", function () {
-            var value = valueAccessor();
-            value(new Date(element.value));
-        });
-    },
-    // Update the control whenever the view model changes
-    update: function (element, valueAccessor, allBindingsAccessor, viewModel) {
-        var value =  valueAccessor();
-        element.value = value().toLocalISOStringNoZ();
-    }
-};
