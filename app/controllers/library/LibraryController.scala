d5885e922ad825b47583d47f010544cbd63cfbe7
Kristian Kime <kristian.kime@gmail.com>
2018-01-21 15:12:17 -0500

    Can now create and answers questions in the library

--- app/controllers/library/LibraryController.scala
+++ app/controllers/library/LibraryController.scala
@@ -5,10 +5,10 @@ import javax.inject.{Inject, Singleton}
 import com.artclod.slick.JodaUTC
 import controllers.Application
 import controllers.organization.CourseJoin
-import controllers.quiz.{QuestionCreate, QuizAvailability}
+import controllers.quiz.{AnswerCreate, AnswerJson, QuestionCreate, QuizAvailability}
 import controllers.support.{Consented, RequireAccess}
 import dao.organization.{CourseDAO, OrganizationDAO}
-import dao.quiz.{QuestionDAO, QuizDAO, SkillDAO}
+import dao.quiz.{AnswerDAO, QuestionDAO, QuizDAO, SkillDAO}
 import dao.user.UserDAO
 import models._
 import models.organization.Course
@@ -23,16 +23,16 @@ import play.api.mvc.Results.Redirect
 import play.api.mvc._
 import play.libs.concurrent.HttpExecutionContext
 import com.artclod.util._
-import models.quiz.QuestionFrame
+import models.quiz.{AnswerFrame, QuestionFrame}
 import play.api.libs.json.{JsError, JsSuccess, Json}
 
 import scala.concurrent.{Await, ExecutionContext, Future}
 import scala.util.{Random, Right}
 
 @Singleton
-class LibraryController @Inject()(val config: Config, val playSessionStore: PlaySessionStore, override val ec: HttpExecutionContext, userDAO: UserDAO, organizationDAO: OrganizationDAO, courseDAO: CourseDAO, quizDAO: QuizDAO, skillDAO: SkillDAO, questionDAO: QuestionDAO)(implicit executionContext: ExecutionContext) extends Controller with Security[CommonProfile] {
+class LibraryController @Inject()(val config: Config, val playSessionStore: PlaySessionStore, override val ec: HttpExecutionContext, userDAO: UserDAO, organizationDAO: OrganizationDAO, courseDAO: CourseDAO, quizDAO: QuizDAO, skillDAO: SkillDAO, questionDAO: QuestionDAO, answerDAO: AnswerDAO)(implicit executionContext: ExecutionContext) extends Controller with Security[CommonProfile] {
 
-  def view() = Secure("RedirectUnauthenticatedClient", "Access") { profiles => Consented(profiles, userDAO) { implicit user => Action.async { implicit request =>
+  def list() = Secure("RedirectUnauthenticatedClient", "Access") { profiles => Consented(profiles, userDAO) { implicit user => Action.async { implicit request =>
     skillDAO.allSkills.flatMap(skills => { questionDAO.skillsForAllSet().map(questionsAndSkills => {
             Ok(views.html.library.list(skills, questionsAndSkills))
       })})
@@ -51,12 +51,61 @@ class LibraryController @Inject()(val config: Config, val playSessionStore: Play
               case JsError(errors) => Future.successful(BadRequest(views.html.errors.jsonErrorPage(errors)))
               case JsSuccess(value, path) => {
                 skillDAO.skillsMap.flatMap(skillsMap => { questionDAO.insert(QuestionFrame(value, user.id, skillsMap)).map(questionFrame => {
-                  Redirect(controllers.library.routes.LibraryController.view())
+                  Redirect(controllers.library.routes.LibraryController.list())
                 })})
               }
             }
           }
         )
+
+  } } }
+
+  def viewQuestion(questionId: QuestionId, answerIdOp: Option[AnswerId]) = Secure("RedirectUnauthenticatedClient", "Access") { profiles => Consented(profiles, userDAO) { implicit user => Action.async { implicit request =>
+    (questionDAO.frameByIdEither(questionId) +& answerDAO.frameByIdEither(questionId, answerIdOp)).map( _ match {
+      case Left(notFoundResult) => notFoundResult
+      case Right((question, answerOp)) => {
+        val answerJson : AnswerJson = answerOp.map(a => AnswerJson(a)).getOrElse(controllers.quiz.AnswerJson.blank(question))
+        Ok(views.html.library.viewQuestion(question, answerJson))
+      }
+    })
+  }}}
+
+
+  def answerQuestion(questionId: QuestionId) = Secure("RedirectUnauthenticatedClient", "Access") { profiles => Consented(profiles, userDAO) { implicit user => Action.async { implicit request =>
+
+    questionDAO.frameByIdEither(questionId).flatMap{ _ match {
+      case Left(notFoundResult) => Future.successful(notFoundResult)
+      case Right(question) =>
+        AnswerCreate.form.bindFromRequest.fold(
+          errors => Future.successful(BadRequest(views.html.errors.formErrorPage(errors))),
+          form => {
+            AnswerCreate.answerFormat.reads(Json.parse(form)) match {
+              case JsError(errors) => Future.successful(BadRequest(views.html.errors.jsonErrorPage(errors)))
+              case JsSuccess(value, path) => {
+                val protoAnswerFrame = AnswerFrame(question, value, user.id)
+
+                if(protoAnswerFrame.correctUnknown) { // Here we are unable to determine if the question was answered correctly so we go back to the page
+                  Future.successful( Ok(views.html.library.viewQuestion(question, AnswerJson(protoAnswerFrame))) )
+                } else {
+                  answerDAO.updateSkillCounts(user.id, questionId, protoAnswerFrame.answer.correct).flatMap( updated => { // Keep track of the in/correct counts for each skill
+                    answerDAO.insert(protoAnswerFrame).map(answerFrame => {
+                      Redirect(controllers.library.routes.LibraryController.viewQuestion(questionId, Some(answerFrame.answer.id)))
+//                      if (answerFrame.answer.correct) { // Here everything was correct
+//                        Redirect(controllers.library.routes.LibraryController.viewQuestion(questionId, Some(answerFrame.answer.id)))
+//                      } else { // Here an answer was wrong so give the user another chance to answer
+//                        Redirect(controllers.library.routes.LibraryController.viewQuestion(questionId, Some(answerFrame.answer.id)))
+//                      }
+
+                    })
+                  })
+                }
+
+              }
+            }
+          }
+        )
+
+    } }
 
   } } }
 
