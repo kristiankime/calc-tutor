cad453e0b9fb7c7c8e31af43e6ac6febae672382
Kristian Kime <kristian.kime@gmail.com>
2019-01-05 17:28:00 -0500

    inc version

--- app/controllers/ApplicationInfo.scala
+++ app/controllers/ApplicationInfo.scala
@@ -3,7 +3,7 @@ package controllers
 import java.util.concurrent.TimeUnit
 
 object ApplicationInfo {
-  val version = Version(0, 0, 2)
+  val version = Version(0, 0, 3)
 
   val appTimeoutNum = 300 // TODO change for production
   val appTimeoutUnit = TimeUnit.SECONDS
